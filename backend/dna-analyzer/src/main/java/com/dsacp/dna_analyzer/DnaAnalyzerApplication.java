8b6e0ee0f1532c99173cc94aa7abdc731c89184b
salkesakshant@gmail.com <salkesakshant@gmail.com>
2025-10-22 12:29:21 +0530

    initialized backend & frontend

---
+++ backend/dna-analyzer/src/main/java/com/dsacp/dna_analyzer/DnaAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.dsacp.dna_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaAnalyzerApplication.class, args);
+	}
+
+}
